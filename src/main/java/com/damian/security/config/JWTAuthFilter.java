968e36aeaa1c6551303b1f20692426dacba92ec7
Damian Peiris. <drpeiris3@gmail.com>
2023-09-29 11:20:53 +0530

    Added missing annotations for JWTAuthFilter class.

--- src/main/java/com/damian/security/config/JWTAuthFilter.java
+++ src/main/java/com/damian/security/config/JWTAuthFilter.java
@@ -4,13 +4,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@Component
+@RequiredArgsConstructor
 public class JWTAuthFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull  HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
 
     }
 }
